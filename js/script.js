c65edbee94111896e64aeab0c16ed8501cbb95e3
Jeong Hoon <gbg9609@naver.com>
2023-09-05 16:46:38 +0900

    ccc

--- js/script.js
+++ js/script.js
@@ -1,57 +1,19 @@
-let computerNum = 0
+let inNum = document.getElementById('num')
+let result = document.getElementById('result')
 
-let goButton = document.getElementById('go-btn')
-let userInput = document.getElementById('user-input')
-let resultArea = document.getElementById('result')
-let resetButton = document.getElementById('reset')
-let chanceArea = document.getElementById('chance-save')
-let chances = 5
-let gameOver = false
-
-console.log(resultArea);
-
-goButton.addEventListener('click', play)
-resetButton.addEventListener('click', rst)
-
-
-function pickRandomNum (){
-  computerNum = Math.floor(Math.random()*100)+1
-  console.log(computerNum);
+  function ranNum(){
+  let setNum = Math.floor(Math.random() * 100)
+  console.log('정답',setNum);
 }
 
-function play(){
-  let userValue = userInput.value
+ranNum()
 
-  chances --;
-  chanceArea.textContent = `남은 기회 :${chances}`
-  console.log('chance', chances);
-
-  if(userValue < computerNum){
-    resultArea.innerHTML = 'up!!'
-  }else if(userValue > computerNum){
-    resultArea.innerHTML = 'down!!'
-  }else{
-    resultArea.innerHTML = '정답입니다!!'
-  } 
-
-  if(chances < 1 ){
-    gameOver = true
-    
-  }
-
-  if(gameOver == true){
-    goButton.disabled = true
-    
-  }
-}
-
-function rst(){
-  userInput.value = ''
-  pickRandomNum()
-  play()
-
-  resultArea.innerHTML = '결과값이 여기 나옵니다!'
-}
-
-pickRandomNum()
+  document.querySelector('.ex1 button').addEventListener('click',e => {
+    let userValue = inNum.value
+    let getResult = result.value
+    if(userValue == ranNum){
+      console.log('정답입니다.');
+    }
+  })
+ddd
 
